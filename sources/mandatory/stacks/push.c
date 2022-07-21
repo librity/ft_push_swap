b2aec78010e1d1a6c9f4d7457dd107fc13144cc2
Luis <luis.geniole@gmail.com>
2022-07-20 21:01:44 -0300

    Abstract push logic.

--- sources/mandatory/stacks/push.c
+++ sources/mandatory/stacks/push.c
@@ -6,61 +6,53 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 00:17:03 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/07/20 18:12:42 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/07/20 20:59:23 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-// static t_dlist	*resolve_stacks(char label)
-// {
-// 	if (label == A_STACK_LABEL)
-// 		return (*a());
-// 	if (label == B_STACK_LABEL)
-// 		return (*b());
-// 	die();
-// }
+static t_dlist	**origin(char label)
+{
+	if (label == A_STACK_LABEL)
+		return (b());
+	if (label == B_STACK_LABEL)
+		return (a());
+	die();
+	return (NULL);
+}
 
-// static void	p(char label)
-// {
-// 	t_dlist	*stack;
-// 	int		*aux;
+static t_dlist	**destination(char label)
+{
+	if (label == A_STACK_LABEL)
+		return (a());
+	if (label == B_STACK_LABEL)
+		return (b());
+	die();
+	return (NULL);
+}
 
-// 	stack = resolve_stack(label);
-// 	if (stack == NULL)
-// 		return ;
-// 	if (stack->next == NULL)
-// 		return ;
-// 	aux = stack->content;
-// 	stack->content = stack->next->content;
-// 	stack->next->content = aux;
-// }
-
-void	pa(void)
+static void	p(char label)
 {
 	t_dlist	*pushed;
 
-	if (*b() == NULL)
+	if (*origin(label) == NULL)
 		return ;
-	pushed = *b();
-	*b() = pushed->next;
-	if (*b() != NULL)
-		(*b())->prev = NULL;
-	ft_dlstadd_front(a(), pushed);
+	pushed = *origin(label);
+	*origin(label) = pushed->next;
+	if (*origin(label) != NULL)
+		(*origin(label))->prev = NULL;
+	ft_dlstadd_front(destination(label), pushed);
+}
+
+void	pa(void)
+{
+	p('a');
 	ft_putendl_fd("pa", STDOUT_FILENO);
 }
 
 void	pb(void)
 {
-	t_dlist	*pushed;
-
-	if (*a() == NULL)
-		return ;
-	pushed = *a();
-	*a() = pushed->next;
-	if (*a() != NULL)
-		(*a())->prev = NULL;
-	// ft_printf("DEGUB: \n");
-	ft_dlstadd_front(b(), pushed);
+	p('b');
 	ft_putendl_fd("pb", STDOUT_FILENO);
 }
